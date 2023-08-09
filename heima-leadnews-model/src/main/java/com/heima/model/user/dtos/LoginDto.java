2689a790d4733ff624839cfdb74a107c978cc6c4
yanghan <948557946@qq.com>
2023-08-09 22:29:48 +0800

    Initial commit

---
+++ heima-leadnews-model/src/main/java/com/heima/model/user/dtos/LoginDto.java
@@ -0,0 +1,24 @@
+package com.heima.model.user.dtos;
+
+
+import com.sun.istack.internal.NotNull;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+public class LoginDto {
+
+    /**
+     * 手机号
+     */
+    @ApiModelProperty(value = "手机号",required = true)
+    @NotNull
+    private String phone;
+
+    /**
+     * 密码
+     */
+    @ApiModelProperty(value = "密码",required = true)
+    @NotNull
+    private String password;
+}
